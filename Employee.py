03be82f77181937dae1c76d863a54fdd941760f2
irwan1398 <121339103+irwan1398@users.noreply.github.com>
2023-06-06 20:45:53 +0700

    Update Employee.py
    merubah cource code

--- Employee.py
+++ Employee.py
@@ -1,4 +1,4 @@
-class Enployee:
+class Employee:
   empCount = 0   
   
   
@@ -8,15 +8,15 @@ class Enployee:
     Employee,empCount +=1
     
     def displayCount(self):
-      print("Total Employee%d %Employee<empcount)
+      print("Total Employee %d %Employee,empcount)
     def displayEmployee(self):
        print ("name:",self,name,",salary",self salary)
             
-    emp1 =Employee("M.Irwan", 1000)
-    emp2 =Employee("arif",7000)
+    emp1 =employee("M.Irwan", 1000)
+    emp2 =employee("arif",7000)
     emp1,displayEmployee()
     emp2,displayEmployee()
-    print ("total Employee %d" %Employee,emCount()        
+    print ("total Employee %d" %employee,emCount)        
      
             
     
