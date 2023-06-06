b64a4a51b9c20ed0897104d12517b7922951479f
irwan1398 <121339103+irwan1398@users.noreply.github.com>
2023-06-06 20:35:10 +0700

    Create Employee.py
    Menambahkan class Employee

---
+++ Employee.py
@@ -0,0 +1,22 @@
+class Enployee:
+  empCount = 0   
+  
+  
+  def_init_(self,name,salary):
+    self,name = name
+    self,salary = salary
+    Employee,empCount +=1
+    
+    def displayCount(self):
+      print("Total Employee%d %Employee<empcount)
+    def displayEmployee(self):
+       print ("name:",self,name,",salary",self salary)
+            
+    emp1 =Employee("M.Irwan", 1000)
+    emp2 =Employee("arif",7000)
+    emp1,displayEmployee()
+    emp2,displayEmployee()
+    print ("total Employee %d" %Employee,emCount()        
+     
+            
+    
